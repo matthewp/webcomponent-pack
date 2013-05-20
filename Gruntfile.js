03eff149dc1d83af53e8049774df1aabc1b7a7dc
Matthew Phillips <matthew@matthewphillips.info>
2013-05-19 22:33:25 -0400

    Added grunt task for tests.

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,16 +2,22 @@ module.exports = function(grunt) {
   grunt.initConfig({
     jshint: {
       files: ['Gruntfile.js', 'index.js', 'bin/**/*.*', 'lib/**/*.js'],
-        options: {
-          es5: true,
-          eqnull: true,
-          laxbreak: true,
-          globals: {
-          }
+      options: {
+        es5: true,
+        eqnull: true,
+        laxbreak: true,
+        globals: {
         }
+      }
+    },
+    simplemocha: {
+      all: { src: ['test/*.js'] }
     }
   });
   grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-simple-mocha');
+
   grunt.registerTask('lint', ['jshint']);
-  grunt.registerTask('default', ['jshint']);
+  grunt.registerTask('test', ['simplemocha']);
+  grunt.registerTask('default', ['jshint', 'test']);
 };
